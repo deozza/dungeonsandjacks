ec1ba446d745526c7cc58b050ced985f431fc56e
Edenn Touitou <edenn.touitou@letsjam.fr>
2025-08-20 00:53:21 +0200

    playerBuilder done and 100 tested

---
+++ src/lib/builders/PlayerBuilder/PlayerBuilder.test.ts
@@ -0,0 +1,177 @@
+import GameLoop from "$lib/gameLoop/GameLoop";
+import { describe, expect, it, vi } from "vitest";
+import PlayerBuilder from "./PlayerBuilder";
+import type { Entity } from "$lib/ECS/entities";
+import DiscardComponent from "$lib/ECS/components/DiscardComponent";
+import HandComponent from "$lib/ECS/components/HandComponent";
+import DeckComponent from "$lib/ECS/components/DeckComponent";
+import HealthComponent from "$lib/ECS/components/HealthComponent";
+import DescriptionComponent from "$lib/ECS/components/DescriptionComponent";
+import NameComponent from "$lib/ECS/components/NameComponent";
+import CardRankComponent from "$lib/ECS/components/CardRankComponent";
+import CardSuitComponent from "$lib/ECS/components/CardSuitComponent";
+
+const values = {
+  NameComponent: {
+    content: 'test name'
+  },
+  DescriptionComponent: {
+    content: 'test description'
+  },
+  HealthComponent: {
+    maxHealth: 100
+  },
+  DeckComponent: {
+    cards: {
+      hearts: [1],
+      aces: [1],
+      diamonds: [1],
+      clubs: [1],
+    }
+  }
+  
+}
+
+describe('buildNameComponent', () => {
+  it('adds NameComponent to entity according to values', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+    
+    const playerEntity: Entity = gameLoop.addEntity();
+
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(0);
+    
+    playerBuilder['buildNameComponent'](values, playerEntity);
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(1);
+    expect(gameLoop.entityHasAllComponents(playerEntity, new Set<Function>([NameComponent])));
+    expect(gameLoop.getComponentFromEntity(NameComponent, playerEntity) as NameComponent).toEqual(values.NameComponent);
+  });
+});
+
+describe('buildDescriptionComponent', () => {
+  it('adds DescriptionComponent to entity according to values', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+    
+    const playerEntity: Entity = gameLoop.addEntity();
+
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(0);
+    
+    playerBuilder['buildDescriptionComponent'](values, playerEntity);
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(1);
+    expect(gameLoop.entityHasAllComponents(playerEntity, new Set<Function>([DescriptionComponent])));
+    expect(gameLoop.getComponentFromEntity(DescriptionComponent, playerEntity) as DescriptionComponent).toEqual(values.DescriptionComponent);
+  });
+});
+
+describe('buildHealthComponent', () => {
+  it('adds HealthComponent to entity according to values', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+
+    const playerEntity: Entity = gameLoop.addEntity();
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(0);
+    
+    playerBuilder['buildHealthComponent'](values, playerEntity);
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(1);
+    expect(gameLoop.entityHasAllComponents(playerEntity, new Set<Function>([HealthComponent])));
+    
+    expect(gameLoop.getComponentFromEntity(HealthComponent, playerEntity) as HealthComponent).toEqual({maxHealth: 100, currentHealth: 100});
+  });
+});
+
+describe('buildDeckComponent', () => {
+  it('adds DeckComponent to entity according to values', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+    
+    const playerEntity: Entity = gameLoop.addEntity();
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(0);
+    
+    playerBuilder['buildDeckComponent'](values, playerEntity);
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(1);
+    expect(gameLoop.entityHasAllComponents(playerEntity, new Set<Function>([DeckComponent])));
+
+    const deckComponent: DeckComponent = gameLoop.getComponentFromEntity(DeckComponent, playerEntity) as DeckComponent;
+    
+    expect(deckComponent.cards.size).toBe(4);
+  });
+});
+
+describe('buildHandComponent', () => {
+  it('adds empty HandComponent', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+
+    const playerEntity: Entity = gameLoop.addEntity();
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(0);
+    
+    playerBuilder['buildHandComponent'](playerEntity);
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(1);
+    expect(gameLoop.entityHasAllComponents(playerEntity, new Set<Function>([HandComponent])));
+    expect(gameLoop.getComponentFromEntity(HandComponent, playerEntity) as HandComponent).toEqual(new HandComponent());
+    
+  });
+});
+
+describe('buildDiscardComponent', () => {
+  it('adds empty DiscardComponent', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+    
+    const playerEntity: Entity = gameLoop.addEntity();
+
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(0);
+    
+    playerBuilder['buildDiscardComponent'](playerEntity);
+    
+    expect(gameLoop.getComponentsFromEntity(playerEntity)?.size).toBe(1);
+    expect(gameLoop.entityHasAllComponents(playerEntity, new Set<Function>([DiscardComponent])));
+    expect(gameLoop.getComponentFromEntity(DiscardComponent, playerEntity) as DiscardComponent).toEqual(new DiscardComponent());
+    
+  });
+});
+
+describe('build', () => {
+  it('throws if no gameLoop', () => {
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+
+    expect(() => playerBuilder.build(values)).toThrow();
+  });
+  
+  it('calls other methods', () => {
+    const gameLoop: GameLoop = new GameLoop();
+    const playerBuilder: PlayerBuilder = new PlayerBuilder();
+    playerBuilder.gameLoop = gameLoop;
+    
+    const buildNameComponentSpy = vi.spyOn(playerBuilder, 'buildNameComponent');
+    const buildDescriptionComponent = vi.spyOn(playerBuilder, 'buildDescriptionComponent');
+    const buildHealthComponent = vi.spyOn(playerBuilder, 'buildHealthComponent');
+    const buildDeckComponent = vi.spyOn(playerBuilder, 'buildDeckComponent');
+    const buildDiscardComponent = vi.spyOn(playerBuilder, 'buildDiscardComponent');
+    const buildHandComponent = vi.spyOn(playerBuilder, 'buildHandComponent');
+
+    playerBuilder.build(values);
+
+    expect(buildNameComponentSpy).toHaveBeenCalledTimes(1);
+    expect(buildDescriptionComponent).toHaveBeenCalledTimes(1);
+    expect(buildHealthComponent).toHaveBeenCalledTimes(1);
+    expect(buildDeckComponent).toHaveBeenCalledTimes(1);
+    expect(buildDiscardComponent).toHaveBeenCalledTimes(1);
+    expect(buildHandComponent).toHaveBeenCalledTimes(1);
+  });
+});
--- src/lib/builders/PlayerBuilder/PlayerBuilder.ts
+++ src/lib/builders/PlayerBuilder/PlayerBuilder.ts
@@ -6,33 +6,96 @@ import HealthComponent from "$lib/ECS/components/HealthComponent";
 import NameComponent from "$lib/ECS/components/NameComponent";
 import type { Entity } from "$lib/ECS/entities";
 import AbstractBuilder from "../AbstractBuilder";
+import CardBuilder from "../CardBuilder/CardBuilder";
 
-type PlayerModele = {
-  
+type PlayerModel = {
+  NameComponent: {
+    content: string
+  },
+  DescriptionComponent: {
+    content: string
+  },
+  HealthComponent: {
+    maxHealth: number
+  },
+  DeckComponent: {
+    cards: {
+      hearts: number[],
+      aces: number[],
+      diamonds: number[],
+      clubs: number[],
+    }
+  }
 };
 
 export default class PlayerBuilder extends AbstractBuilder {
-    public build(values: PlayerModele): Entity {
+    public build(values: PlayerModel): Entity {
       if(this.gameLoop === undefined) {
         throw 'gameLoop must be instanciated before using a builder';
       }
       
       const playerEntity: Entity = this.gameLoop.addEntity();
-      
-      const deckComponent: DeckComponent = new DeckComponent();
-      const discardComponent: DiscardComponent = new DiscardComponent();
-      const handComponent: HandComponent = new HandComponent();
-      const healthComponent: HealthComponent = new HealthComponent();
-      const descriptionComponent: DescriptionComponent = new DescriptionComponent();
-      const nameComponent: NameComponent = new NameComponent();
 
-      this.gameLoop.addComponentToEntity(deckComponent, playerEntity);
-      this.gameLoop.addComponentToEntity(discardComponent, playerEntity);
-      this.gameLoop.addComponentToEntity(handComponent, playerEntity);
-      this.gameLoop.addComponentToEntity(healthComponent, playerEntity);
-      this.gameLoop.addComponentToEntity(descriptionComponent, playerEntity);
-      this.gameLoop.addComponentToEntity(nameComponent, playerEntity);
+      this.buildNameComponent(values, playerEntity);
+      this.buildDescriptionComponent(values, playerEntity);
+      this.buildHealthComponent(values, playerEntity);
+      this.buildDeckComponent(values, playerEntity);
+      this.buildHandComponent(playerEntity);
+      this.buildDiscardComponent(playerEntity);
 
       return playerEntity;
+    }
+
+    private buildNameComponent(values: PlayerModel, playerEntity: Entity): void {
+      const nameComponent: NameComponent = new NameComponent();
+      nameComponent.content = values.NameComponent.content;
+
+      this.gameLoop?.addComponentToEntity(nameComponent, playerEntity);
+    }
+
+    private buildDescriptionComponent(values: PlayerModel, playerEntity: Entity): void {
+      const descriptionComponent: DescriptionComponent = new DescriptionComponent();
+      descriptionComponent.content = values.DescriptionComponent.content;
+
+      this.gameLoop?.addComponentToEntity(descriptionComponent, playerEntity);
+      
+    }
+
+    private buildHealthComponent(values: PlayerModel, playerEntity: Entity): void {
+      const healthComponent: HealthComponent = new HealthComponent();
+      healthComponent.maxHealth = values.HealthComponent.maxHealth;
+      healthComponent.currentHealth = healthComponent.maxHealth;
+
+      this.gameLoop?.addComponentToEntity(healthComponent, playerEntity);
+      
+    }
+
+    private buildDeckComponent(values: PlayerModel, playerEntity: Entity): void {
+      const cardBuilder: CardBuilder = new CardBuilder();
+      cardBuilder.gameLoop = this.gameLoop;
+      
+      const deckComponent: DeckComponent = new DeckComponent();
+      for(const suit in values.DeckComponent.cards) {
+        for(const rank of values.DeckComponent.cards[suit]) {
+          const cardEntity: Entity  = cardBuilder.build({cardRank: rank, cardSuit: suit})
+
+          deckComponent.cards.add(cardEntity);
+        }
+      }
+
+      this.gameLoop?.addComponentToEntity(deckComponent, playerEntity);
+    }
+
+    private buildHandComponent(playerEntity: Entity): void {
+      const handComponent: HandComponent = new HandComponent();
+      
+      this.gameLoop?.addComponentToEntity(handComponent, playerEntity);
+    }
+
+    private buildDiscardComponent(playerEntity: Entity): void {
+      const discardComponent: DiscardComponent = new DiscardComponent();
+      
+      this.gameLoop?.addComponentToEntity(discardComponent, playerEntity);
+      
     }
 }
